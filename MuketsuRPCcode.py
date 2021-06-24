59ec0961c5e9686e188e3515820a12c177f040f7
Wet Mop <34312571+WetMop@users.noreply.github.com>
2021-06-24 05:28:02 -0400

    Update MuketsuRPCcode.py

--- MuketsuRPCcode.py
+++ MuketsuRPCcode.py
@@ -1,7 +1,7 @@
 ##############READ###############
 # Hello :)
 
-# I will mark Changable options with a comment in green just like this
+# I will mark Changable options with a comment in gray just like this
 
 # When you change a option you must change the word inside the "" 
 
@@ -18,12 +18,12 @@ client_id = '426143265671086122'
 RPC = Presence(client_id) 
 RPC.connect()
 
-print(RPC.update(state="Chillin", 
-                 details="Who are you?",
+print(RPC.update(state="Chillin", # Optional
+                 details="Who are you?", # Optional
                  large_image="muketsu",
-                 large_text="discord.gg/muketsu", 
+                 large_text="discord.gg/muketsu", #Optional
                  small_image="discord", 
-                 small_text="Chillin on Discord", 
+                 small_text="Chillin on Discord", #Optional
                  buttons=[{"label": "Join Muketsu", "url":"https://discord.com/invite/muketsu"}] )) 
 
 # Hellow :) 
